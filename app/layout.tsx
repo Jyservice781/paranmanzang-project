a7e00339ab79fd16e6cc72559d7d1085bdd6a54d
Jyservice781 <gimjuyeong512@gmail.com>
2024-10-22 12:01:51 +0900

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ export const metadata: Metadata = {
   }
 };
 
-export default function RootLayout({
+export default function RootLayout({   
   children,
 }: Readonly<{
   children: React.ReactNode;
