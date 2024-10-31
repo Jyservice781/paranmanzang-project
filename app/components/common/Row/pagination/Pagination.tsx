d4056332a60bdb1b4dd9be3e9b51592fe215e8e1
Jyservice781 <gimjuyeong512@gmail.com>
2024-11-01 00:44:40 +0900

    update

--- app/components/common/Row/pagination/Pagination.tsx
+++ app/components/common/Row/pagination/Pagination.tsx
@@ -71,10 +71,10 @@ function Pagination({
           <path fillRule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clipRule="evenodd" />
         </svg>
       </button>
-      {/* {renderPageDots()} */}
-      <span className="mx-2 text-sm font-medium text-gray-700">
+      {renderPageDots()} 
+      {/* <span className="mx-2 text-sm font-medium text-gray-700">
         {currentPage + 1} / {totalPages}
-      </span>
+      </span> */}
       <button
         onClick={() => handlePageChange(currentPage + 1)}
         disabled={currentPage === totalPages - 1}
