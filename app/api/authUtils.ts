7293eeab91d3f90840a0f9a389e0b972dc2906ad
Jyservice781 <gimjuyeong512@gmail.com>
2024-11-05 15:02:40 +0900

    update | cookie set update CSRF update

--- app/api/authUtils.ts
+++ app/api/authUtils.ts
@@ -1,7 +1,8 @@
 import Cookies from 'js-cookie';
 
 export const getAccessToken = () => Cookies.get('accessToken')
-export const setAccessToken = (token: string) => Cookies.set('accessToken', token, { secure: true, sameSite: 'strict' })
+export const setAccessToken = (token: string) => Cookies.set('accessToken', token, { secure: true, sameSite: 'Lax' })
+
 export const getNickname = () => Cookies.get('nickname')
 export const getAuthorization = () => Cookies.get('Authorization')
 export const getRefreshToken = () => Cookies.get('refreshToken')
@@ -11,4 +12,4 @@ export const removeNickname = () => Cookies.remove('nickname',{path:'/',domain: 
 export const removeAuthorization = () => Cookies.remove('Authorization',{path:'/',domain: 'paranmanzang.com'})
 export const removeRefreshToken = () => Cookies.remove('refreshToken')
 
-export const setRefreshToken = (token: string) => Cookies.set('refreshToken', token, { secure: true, sameSite: 'strict' })
+export const setRefreshToken = (token: string) => Cookies.set('refreshToken', token, { secure: true, sameSite: 'Lax' })
